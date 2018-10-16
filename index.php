ec5d2e3813168574d255d544695f81a4dcb9b719
Vytautas <vytautas.1717@gmail.com>
2018-10-17 01:30:39 +0300

    Pirmas puslapis

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php
-print 'Veikia su Github';
+<html>
+<body>
 
-?>
+<h1>Welcome to my home page!</h1>
+<p>Some text.</p>
+<p>Some more text.</p>
+<?php include 'footer.php';?>
+
+</body>
+</html>
