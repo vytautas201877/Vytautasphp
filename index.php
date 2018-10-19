0e36337c828432a3ad03b690f5411ac5550ce01c
Vytautas <vytautas.1717@gmail.com>
2018-10-19 16:01:38 +0300

    saldytuvas tikrina pagal mano sarasa ir ismeta is saraso

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php
-$fridge = ["Jogurtas", "Kebabas", "Alus", "Sugede vaisiai", "supuves avokadas"];
+$fridge = array("Jogurtas", "Kebabas", "Alus", "Sugede vaisiai", "supuves avokadas");
 $noriu = ["Kebabas", "Alus", "Pica", "traktorius"];
-$tekstas = null;
-foreach ($noriu as $kazkas) {
-    if (in_array ($kazkas, $fridge)) {
-        $tekstas .= "$kazkas: Turiu \n";
-    } else {
-        $tekstas .= "$kazkas: Neturiu :-) \n";
+$tekstas = '';
+foreach ($fridge as $nr => $kazkas) {
+    if (in_array($kazkas, $noriu)) {
+        unset ($fridge[$nr]);
     }
 }
-?>
+foreach ($fridge as $ka){
+    $tekstas.=$ka;
+}
+var_dump($fridge)
+ ?>
+    
 <html>
     <body>
         <h1>Ar viska turiu saldytuve?</h1>
-        <p><?php print nl2br($tekstas); ?></p>
+        <p><?php print ($tekstas); ?></p>
     </body>
 </html>
