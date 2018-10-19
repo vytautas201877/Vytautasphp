89cff84de7b2e3951b4a676243089ff3f5695022
Vytautas <vytautas.1717@gmail.com>
2018-10-19 15:36:04 +0300

    saldytuvas tikrina pagal mano sarasa

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
-$fridge = ['Jogurtas', 'Kebabas', 'Alus', 'Sugede vaisiai', 'Supuves avokadas'];
-$i = rand(0, 4);
-$text = $fridge[$i].'=elementas======'.$i ;
-//$text .= $i;
+$fridge = ["Jogurtas", "Kebabas", "Alus", "Sugede vaisiai", "supuves avokadas"];
+$noriu = ["Kebabas", "Alus", "Pica", "traktorius"];
+$tekstas = null;
+foreach ($noriu as $kazkas) {
+    if (in_array ($kazkas, $fridge)) {
+        $tekstas .= "$kazkas: Turiu \n";
+    } else {
+        $tekstas .= "$kazkas: Neturiu :-) \n";
+    }
+}
 ?>
 <html>
     <body>
-        <h1>Saldytuvo Turinys</h1>
-        <p><?php print $text ?></p>
+        <h1>Ar viska turiu saldytuve?</h1>
+        <p><?php print nl2br($tekstas); ?></p>
     </body>
 </html>
