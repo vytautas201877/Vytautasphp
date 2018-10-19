33ced7c71da25853b8f11cc6bcb859dd8cbd4136
Vytautas <vytautas.1717@gmail.com>
2018-10-19 15:19:28 +0300

    saldytuvas rand

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 $fridge = ['Jogurtas', 'Kebabas', 'Alus', 'Sugede vaisiai', 'Supuves avokadas'];
-for($i = 0; $i < count($fridge); $i++)
-{
-    print $fridge[$i] . '          ';
-}
+$i = rand(0, 4);
+$text = $fridge[$i].'=elementas======'.$i ;
+//$text .= $i;
+?>
+<html>
+    <body>
+        <h1>Saldytuvo Turinys</h1>
+        <p><?php print $text ?></p>
+    </body>
+</html>
