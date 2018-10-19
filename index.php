3eb3a1a155ca387a07307aa421f852ed250e14b6
Vytautas <vytautas.1717@gmail.com>
2018-10-19 14:29:52 +0300

    saldytuvas php

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
-$fridge= array("Jogurtas", "Kebabas", "Alus", "Sugede vaisiai", "supuves avokadas");
-?>
-<html>
-    <body>
-        <h1>Saldytuvo turinys</h1>
-        <?php for($i = 0; $i<5;$i++):?>
-        <p><?php print $fridge[$i];?></p>
-        <?php endfor;?>
-    </body>
-</html>
+$fridge = ['Jogurtas', 'Kebabas', 'Alus', 'Sugede vaisiai', 'Supuves avokadas'];
+for($i = 0; $i < count($fridge); $i++)
+{
+    print $fridge[$i] . '          ';
+}
