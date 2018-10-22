b9a59dea55c07799f0d8bc78c9e27e498edefff6
Vytautas <vytautas.1717@gmail.com>
2018-10-22 14:56:40 +0300

    ideta i atskirus div

--- index.php
+++ index.php
@@ -1,18 +1,32 @@
 <?php
-$Fridge = [
-    "pienas" => 1,
-    "duona" => 2,
-    "agurkas" => 3,
-    "kiausai" => 5
-];
-$index = array_keys($Fridge);
-$randindx = rand(0, count($index) - 1);
-$randFridgeElement = $index[$randindx];
-$text = "Produktas=" . $randFridgeElement . "-" . $Fridge[$randFridgeElement] . "eur";
+$products = ['kebabas' => 2.50, 'alus' => 1.89, 'burokai' => 1.50];
+
 ?>
+
 <html>
+    <head>
+        <title>Produktai</title>
+        <style>
+            .products {
+                width: 150px;
+                height: 150px;
+            }
+            .kebabas {
+                background-image: url(https://foodout.lt/uploads/dishes/thumb_type1_noid_151231105642_758808d6c22af0b9af63df51a31d898f.png);
+            }
+            .alus {
+                background-image: url(https://g2.dcdn.lt/images/pix/file59627425_6747ce14.jpg);
+            }
+            .burokai {
+                background-image: url(http://www.dietosmeniu.lt/data/ingredients/large/burokeliai.jpg);
+            }
+        </style>
+    </head>
     <body>
-        <h1>Ar viska turiu saldytuve?</h1>
-        <p><?php print $text ?></p>
+        <?php foreach($products as $key => $value): ?>
+        <div class="products <?php print $key; ?>">
+            <?php print $value; ?>
+        </div>
+        <?php endforeach; ?>
     </body>
 </html>
