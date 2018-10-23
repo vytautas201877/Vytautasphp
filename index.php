d56b408714457219a5e18b5bc5d7a1b31fb3a4bd
Vytautas <vytautas.1717@gmail.com>
2018-10-23 13:19:30 +0300

    panos

--- index.php
+++ index.php
@@ -1,32 +1,51 @@
 <?php
-$products = ['kebabas' => 2.50, 'alus' => 1.89, 'burokai' => 1.50];
+
+$panos = [
+    [
+        'vardas' => 'Monika',
+        'Grazi' => true,
+        'protinga' => false
+    ],
+    [
+        'vardas' => 'Greta',
+        'Grazi' => true,
+        'protinga' => true
+    ],
+    [
+        'vardas' => 'Laura',
+        'Grazi' => false,
+        'protinga' => true
+    ],
+    [
+        'vardas' => 'Viktorija',
+        'Grazi' => false,
+        'protinga' => true
+    ],
+    [
+        'vardas' => 'Aldona',
+        'Grazi' => true,
+        'protinga' => true
+    ]
+    
+];
+
+function girls($panos){
+    $geros = [];
+    foreach($panos as $pana){
+        if($pana['Grazi'] && $pana['protinga']){
+            $geros[] = $pana['vardas'];
+        }
+    }
+    return $geros;
+}
+//$geros = girls($panos);
 
 ?>
 
 <html>
-    <head>
-        <title>Produktai</title>
-        <style>
-            .products {
-                width: 150px;
-                height: 150px;
-            }
-            .kebabas {
-                background-image: url(https://foodout.lt/uploads/dishes/thumb_type1_noid_151231105642_758808d6c22af0b9af63df51a31d898f.png);
-            }
-            .alus {
-                background-image: url(https://g2.dcdn.lt/images/pix/file59627425_6747ce14.jpg);
-            }
-            .burokai {
-                background-image: url(http://www.dietosmeniu.lt/data/ingredients/large/burokeliai.jpg);
-            }
-        </style>
-    </head>
     <body>
-        <?php foreach($products as $key => $value): ?>
-        <div class="products <?php print $key; ?>">
-            <?php print $value; ?>
-        </div>
+        <?php foreach(girls($panos) as $var): ?>
+        <h2><?php print $var; ?> </h2>
         <?php endforeach; ?>
     </body>
 </html>
