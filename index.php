98fa8f4e8c5a68ac04bd4167ab2571b7e396fc5b
Vytautas <vytautas.1717@gmail.com>
2018-10-23 15:59:50 +0300

    form

--- index.php
+++ index.php
@@ -1,29 +1,17 @@
 <?php
-$fridge = [];
-
-$nusipirkau = ['cola' => '2.50', 'viskis' => '10.99', 'vodka' => '5.99', 'kiwi' => '1.99', 'citrina' => '0.99', 'alus' => '1.99'];
-$pirkau = ['cola' => '2.50', 'kiwi' => '1.99', 'citrina' => '0.99'];
-
-function shuffle_assoc($my_array) {
-    $keys = array_keys($my_array);
-
-    shuffle($keys);
-
-    foreach ($keys as $key) {
-        $new[$key] = $my_array[$key];
-    }
-    return $new;
-}
+$text=vardas; 
 ?>
 <html>
+    <style>
+       
+    </style>
     <body>
-        <h2>
-            Produktus i saldytuva sudejau tokia tvarka:
-        </h2>
-        <?php foreach (shuffle_assoc($pirkau) as $key => $value) : ?>
-            <div>
-                <?php print "$key uz $value"; ?>
-            </div>
-        <?php endforeach; ?>
-    </body>
+        <form action="index.php" method="POST">
+  Vardas:<br>
+  <input name="vardas" type="text" ><br>
+  
+  <input type="submit">
+</form>
+        <?php print $text?>
+    </body> 
 </html>
