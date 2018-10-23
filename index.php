ba1c5844fb4f6eadc3a29dabd2ff1f98340efe8b
Vytautas <vytautas.1717@gmail.com>
2018-10-23 15:06:47 +0300

    random fridge

--- index.php
+++ index.php
@@ -1,46 +1,29 @@
 <?php
+$fridge = [];
 
-$panos = [
-    [
-        'vardas' => 'Monika',
-        'Grazi' => true,
-        'protinga' => false
-    ],
-    [
-        'vardas' => 'Greta',
-        'Grazi' => true,
-        'protinga' => true
-    ],
-    [
-        'vardas' => 'Laura',
-        'Grazi' => false,
-        'protinga' => true
-    ],
-    [
-        'vardas' => 'Viktorija',
-        'Grazi' => false,
-        'protinga' => true
-    ],
-    [
-        'vardas' => 'Aldona',
-        'Grazi' => true,
-        'protinga' => true
-    ]
-    
-];
+$nusipirkau = ['cola' => '2.50', 'viskis' => '10.99', 'vodka' => '5.99', 'kiwi' => '1.99', 'citrina' => '0.99', 'alus' => '1.99'];
+$pirkau = ['cola' => '2.50', 'kiwi' => '1.99', 'citrina' => '0.99'];
 
-function random($panos)
-{
-    $random = rand(0, count($panos)-1);
-    return $panos[$random]['vardas'];
-    
+function shuffle_assoc($my_array) {
+    $keys = array_keys($my_array);
+
+    shuffle($keys);
+
+    foreach ($keys as $key) {
+        $new[$key] = $my_array[$key];
+    }
+    return $new;
 }
 ?>
-
 <html>
     <body>
-        
-        <h2><?php print random($panos) ?> </h2>
-        
+        <h2>
+            Produktus i saldytuva sudejau tokia tvarka:
+        </h2>
+        <?php foreach (shuffle_assoc($pirkau) as $key => $value) : ?>
+            <div>
+                <?php print "$key uz $value"; ?>
+            </div>
+        <?php endforeach; ?>
     </body>
 </html>
