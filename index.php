8b72a5c7934c6cfa3e1dc62cd2f985871b8d04ca
Vytautas <vytautas.1717@gmail.com>
2018-10-24 13:28:03 +0300

    veikianti forma

--- index.php
+++ index.php
@@ -1,17 +1,27 @@
 <?php
-$text=vardas; 
+$text = "Iveskite skaiciu";
+if (isset($_POST["vardas"])) {
+    $vardas = $_POST["vardas"];
+
+    $text = "pakelta kvadratu=" . kvadratas($vardas);
+} else {
+    echo "Neivesti duomenys";
+}
+function kvadratas($vardas) {
+    return $vardas ** 2;
+}
 ?>
 <html>
     <style>
-       
+
     </style>
     <body>
         <form action="index.php" method="POST">
-  Vardas:<br>
-  <input name="vardas" type="text" ><br>
-  
-  <input type="submit">
-</form>
-        <?php print $text?>
+            <h1> Skaicius:<br></h1>
+            <input name="vardas" type="text" ><br>
+
+            <input type="submit">
+        </form>
+        <?php print $text ?>
     </body> 
 </html>
